9dfd28913886d37e263f732ad260e354f5654176
Ryan Mahoney <ryan@virtuecenter.com>
2020-07-22 17:20:12 -0400

    changes based on review

--- src/lib/d.ts
+++ src/lib/d.ts
@@ -27,39 +27,52 @@ interface ReferenceData {
   is_hispanic_latinx: LatinxPercents;
 }
 
-type GenderResponse = "male" | "female" | "other" | "no";
+enum LatinxResponse {"yes", "no", "Prefer not to say"}
 
-type RaceResponse =
-  | "Asian"
-  | "Black or African American"
-  | "Native American or Alaskan Native"
-  | "Native Hawaiian or Other Pacific Islander"
-  | "White"
-  | "Other"
-  | "Prefer not to say";
+enum GenderResponse {
+  "male",
+  "female",
+  "other",
+  "no",
+}
 
-type EmploymentTypeResponse = "part" | "full";
+enum RaceResponse {
+  "Asian",
+  "Black or African American",
+  "Native American or Alaskan Native",
+  "Native Hawaiian or Other Pacific Islander",
+  "White",
+  "Other",
+  "Prefer not to say",
+}
 
-type CompensationRangeResponse =
-  | "0 - 20,000"
-  | "20,001 - 40,000"
-  | "40,001 - 60,000"
-  | "60,001 - 80,000"
-  | "80,001 - 100,000"
-  | "100,001 - 120,000"
-  | "120,001 - 140,000"
-  | "140,001 - 160,000"
-  | "160,001 - 180,000"
-  | "180,001 - 200,000"
-  | "200,001 - 220,000"
-  | "220,001 - 240,000"
-  | "240,001 - 260,000"
-  | "260,001 - 280,000"
-  | "Above 280,000";
+enum EmploymentTypeResponse {
+  "part",
+  "full",
+}
+
+enum CompensationRangeResponse {
+  "0 - 20,000",
+  "20,001 - 40,000",
+  "40,001 - 60,000",
+  "60,001 - 80,000",
+  "80,001 - 100,000",
+  "100,001 - 120,000",
+  "120,001 - 140,000",
+  "140,001 - 160,000",
+  "160,001 - 180,000",
+  "180,001 - 200,000",
+  "200,001 - 220,000",
+  "220,001 - 240,000",
+  "240,001 - 260,000",
+  "260,001 - 280,000",
+  "Above 280,000",
+}
 
 interface SurveyRecord {
   gender: GenderResponse;
   race: RaceResponse;
+  latinx: LatinxResponse,
   employment_type: EmploymentTypeResponse;
   work_category: string;
   tenure: number;
