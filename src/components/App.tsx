bee0a31f9a609afd57905aff1ad0863f498b8a60
Ryan Mahoney <ryan@virtuecenter.com>
2020-07-21 21:50:30 -0400

    show reading global variable from external page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,16 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
 import "./../assets/scss/App.scss";
 
-class App extends React.Component<Record<string, unknown>, undefined> {
-  public render() {
-    return (
-      <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        <img src={reactLogo.default} height="480" />
-      </div>
-    );
-  }
+declare var dashboardData: any;
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello {dashboardData.name}!</h1>
+    </div>
+  );
 }
 
 declare let module: Record<string, unknown>;
