910de1439cf408df3b5f34663cc6b049b63d67a9
Ian Westcott <iwestcott@mbta.com>
2020-07-22 22:37:40 -0400

    update env vars to match Semaphore's expectations

--- semaphore/deploy.sh
+++ semaphore/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 yarn run build
-aws s3 sync "./dist/" "s3://${S3_BUCKET}" --acl public-read --delete
+aws s3 sync "${S3_DIRECTORY}" "s3://${S3_BUCKET_NAME}" --acl public-read --delete
