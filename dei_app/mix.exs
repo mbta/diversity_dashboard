43b0757ae68bc00fb91b3b4eec05972e2b41cabd
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-09-16 10:33:31 -0400

    Require floki for test
    This is needed for live view tests.

--- dei_app/mix.exs
+++ dei_app/mix.exs
@@ -47,7 +47,8 @@ defmodule DeiApp.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:guardian, "~> 2.0"},
-      {:argon2_elixir, "~> 2.0"}
+      {:argon2_elixir, "~> 2.0"},
+      {:floki, "~> 0.28.0", only: :test}
     ]
   end
 
